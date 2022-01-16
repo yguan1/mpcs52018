03506bbc23fc8f66eff3ff7de66c051c14fec510
Yuxin Guan <yguan1@cs.uchicago.edu>
2022-01-15 19:02:18 -0600

    updated mxm

--- examples/mxm.c
+++ examples/mxm.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<papi.h>
 
 #define N 1000
 
 double x[N][N];
 double y[N][N];
 double z[N][N];
+
+void handle_error(int errcode){
+  char error_str[PAPI_MAX_STR_LEN];
+  //  PAPI_perror(errcode,error_str,PAPI_MAX_STR_LEN);
+  fprintf(stderr,"PAPI_error in call to create_eventset %d: %s\n",errcode,error_str);
+  exit(1);
+}
 
 double timer(void){
   struct timeval time;
@@ -17,7 +25,14 @@ double timer(void){
 int main(int argc, char **argv){
   int i,j,k;
   double r;
+  long long *values;               /* PAPI results array */
   double ts,tf;
+  int Events[] = {PAPI_L1_DCM, PAPI_LD_INS};
+  int ret
+  num_events = sizeof(Events)/sizeof(int);  /* size of event array */
+  event_name = (char *) malloc(128);        /* string name for event, reuse buffer */
+  float rtime, ptime, mflops;
+  long long flpops; 
 
 
   
@@ -29,6 +44,9 @@ int main(int argc, char **argv){
     }
   }
 
+  if ((ret=PAPI_start_counters(Events, num_events)) != PAPI_OK)
+    handle_error(ret);
+  PAPI_flops(&rtime, &ptime, &flpops, &mflops);
   ts = timer();
   for (i=0;i<N;++i){
     for (j=0;j<N;++j){
@@ -39,6 +57,15 @@ int main(int argc, char **argv){
     }
   }
   tf = timer();
-
+  PAPI_flops(&rtime, &ptime, &flpops, &mflops);
+  if ((ret=PAPI_stop_counters(values, num_events)) != PAPI_OK)
+    handle_error(ret);
+  
+  for (i=0;i<num_events;++i){       /* print name/value of each event counter */
+    PAPI_event_code_to_name(Events[i],event_name);
+    printf("%s:%lld\n", event_name, values[i]);
+  }
   printf("%f %f(s)\n", x[0][0],tf-ts);
+  printf("Number of floatinig point operations = %lld\n",flpops);
+  printf("mflops = %f\n",mflops);
 }
--- examples/mxm_tiled.c
+++ examples/mxm_tiled.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<papi.h>
 #define N 1000
 #define MAX( a, b ) ( ( a > b) ? a : b )
 #define MIN( a, b ) ( ( a < b) ? a : b )
@@ -8,6 +9,13 @@
 double x[N][N];
 double y[N][N];
 double z[N][N];
+
+void handle_error(int errcode){
+  char error_str[PAPI_MAX_STR_LEN];
+  //  PAPI_perror(errcode,error_str,PAPI_MAX_STR_LEN);
+  fprintf(stderr,"PAPI_error in call to create_eventset %d: %s\n",errcode,error_str);
+  exit(1);
+}
 
 double timer(void){
   struct timeval time;
@@ -20,7 +28,14 @@ int main(int argc, char **argv){
   int ii,jj,kk;
   int B;
   double r;
+  long long *values;               /* PAPI results array */
   double ts,tf;
+  int Events[] = {PAPI_L1_DCM, PAPI_LD_INS};
+  int ret
+  num_events = sizeof(Events)/sizeof(int);  /* size of event array */
+  event_name = (char *) malloc(128);        /* string name for event, reuse buffer */
+  float rtime, ptime, mflops;
+  long long flpops; 
 
   if (argc < 2)
     B = 16;
@@ -36,6 +51,9 @@ int main(int argc, char **argv){
     }
   }
 
+  if ((ret=PAPI_start_counters(Events, num_events)) != PAPI_OK)
+    handle_error(ret);
+  PAPI_flops(&rtime, &ptime, &flpops, &mflops);
   ts = timer();
   for (jj=0;jj<N;jj+=B){
     for (kk=0;kk<N;kk+=B){
@@ -50,6 +68,15 @@ int main(int argc, char **argv){
     }
   }
   tf = timer();
+  PAPI_flops(&rtime, &ptime, &flpops, &mflops);
+  if ((ret=PAPI_stop_counters(values, num_events)) != PAPI_OK)
+    handle_error(ret);
   
+  for (i=0;i<num_events;++i){       /* print name/value of each event counter */
+    PAPI_event_code_to_name(Events[i],event_name);
+    printf("%s:%lld\n", event_name, values[i]);
+  }
   printf("%f %f(s)\n", x[0][0],tf-ts);
+  printf("Number of floatinig point operations = %lld\n",flpops);
+  printf("mflops = %f\n",mflops);
 }
