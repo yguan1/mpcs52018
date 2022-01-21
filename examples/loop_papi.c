a4dfff8daebbae79746a9fbe84a4c4d372d38b04
Yuxin Guan <yguan1@cs.uchicago.edu>
2022-01-21 15:52:12 -0600

    update

--- examples/loop_papi.c
+++ examples/loop_papi.c
@@ -19,7 +19,15 @@ double addmul(double add, double mul, int ops){
    return  sum1+sum2+sum3+sum4+sum5+mul1+mul2+mul3+mul4+mul5 - expected;
 }
 
+double timer(void){
+  struct timeval time;
+  gettimeofday(&time, 0);
+  return time.tv_sec + time.tv_usec/1000000.0;
+}
+
 int main(int argc, char** argv) {
+   double ts,tf;
+   int ret
    if (argc != 2) {
       printf("usage: %s <num>\n", argv[0]);
       printf("number of operations: <num> millions\n");
@@ -31,10 +39,14 @@ int main(int argc, char** argv) {
 
    double x = M_PI;
    double y = 1.0 + 1e-8;
-
-   int ret = PAPI_hl_region_begin("loop");
+   
+   ts = timer();
+   ret = PAPI_hl_region_begin("loop");
    x = addmul(x, y, n);
    ret = PAPI_hl_region_end("loop");
+   tf = timer();
+
+   printf("clock time: %f(s)\n", x[0][0],tf-ts);
    
    return EXIT_SUCCESS;
 }
