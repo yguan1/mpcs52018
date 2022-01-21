593a0c6cf0a3f755385b937acf2e279798c0564d
Yuxin Guan <yguan1@cs.uchicago.edu>
2022-01-21 16:29:11 -0600

    update

--- examples/loop_papi.c
+++ examples/loop_papi.c
@@ -4,19 +4,39 @@
 #include <sys/time.h>
 #include <papi.h>
 
-double addmul(double add, double mul, int ops){
+double addmul(int ops){
    // Need to initialise differently otherwise compiler might optimise away
    double sum1=0.1, sum2=-0.1, sum3=0.2, sum4=-0.2, sum5=0.0;
    double mul1=1.0, mul2= 1.1, mul3=1.2, mul4= 1.3, mul5=1.4;
+   double add1=M_PI, add2=M_2_PI, add3=M_PI_2, add4=M_PI_4, add5=M_1_PI;
    int loops=ops/10;          // We have 10 floating point operations inside the loop
-   double expected = 5.0*add*loops + (sum1+sum2+sum3+sum4+sum5)
-               + pow(mul,loops)*(mul1+mul2+mul3+mul4+mul5);
 
    for (int i=0; i<loops; i++) {
-      mul1*=mul; mul2*=mul; mul3*=mul; mul4*=mul; mul5*=mul;
-      sum1+=add; sum2+=add; sum3+=add; sum4+=add; sum5+=add;
+      mul1*=add1; sum1+=add1;
+      mul2*=add2; sum2+=add2;
+      mul3*=add3; sum3+=add3;
+      mul4*=add4; sum4+=add4;
+      mul5*=add5; sum5+=add5;
+
+      mul1*=add5; sum1+=add1;
+      mul2*=add4; sum2+=add2;
+      mul3*=add3; sum3+=add3;
+      mul4*=add2; sum4+=add4;
+      mul5*=add1; sum5+=add5;
+
+      mul1*=add1; sum1+=add5;
+      mul2*=add2; sum2+=add4;
+      mul3*=add3; sum3+=add3;
+      mul4*=add4; sum4+=add2;
+      mul5*=add5; sum5+=add1;
+
+      mul1*=add5; sum1+=add5;
+      mul2*=add4; sum2+=add4;
+      mul3*=add3; sum3+=add3;
+      mul4*=add2; sum4+=add2;
+      mul5*=add1; sum5+=add1;
    }
-   return  sum1+sum2+sum3+sum4+sum5+mul1+mul2+mul3+mul4+mul5 - expected;
+   return 0;
 }
 
 double timer(void){
@@ -36,17 +56,14 @@ int main(int argc, char** argv) {
    int n = atoi(argv[1]) * 1000000;
    if (n<=0)
        n=1000;
-
-   double x = M_PI;
-   double y = 1.0 + 1e-8;
    
    ts = timer();
    ret = PAPI_hl_region_begin("loop");
-   x = addmul(x, y, n);
+   x = addmul(n);
    ret = PAPI_hl_region_end("loop");
    tf = timer();
 
-   printf("clock time: %f(s)\n", x[0][0],tf-ts);
+   printf("clock time: %f(s)\n",tf-ts);
    
    return EXIT_SUCCESS;
 }
